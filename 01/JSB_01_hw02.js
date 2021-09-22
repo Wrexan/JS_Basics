6b7488c524550192abbfe99407eebf07e26691cf
Wrexan <fwrexan@gmail.com>
2021-09-22 11:53:26 +0300

    add hw02

---
+++ 01/JSB_01_hw02.js
@@ -0,0 +1,6 @@
+// 2. Объявить две переменные: admin и name. Записать в name строку "Василий"
+// Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
+
+var admin; var name = 'Василий'
+admin = name
+alert(admin)
